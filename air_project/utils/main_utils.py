75ac49beb6c64e0d87390662458a2ea3f98701e3
sweta234 <77224816+sweta234@users.noreply.github.com>
2025-02-23 15:41:30 +0530

    Update main_utils.py

--- air_project/utils/main_utils.py
+++ air_project/utils/main_utils.py
@@ -29,7 +29,7 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         with open(file_path, "w") as file:
             yaml.dump(content, file)
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
     
 
 
@@ -47,7 +47,7 @@ def load_object(file_path: str) -> object:
         return obj
 
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
     
 
 
@@ -63,7 +63,7 @@ def save_numpy_array_data(file_path: str, array: np.array):
         with open(file_path, 'wb') as file_obj:
             np.save(file_obj, array)
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
     
 
 
@@ -78,7 +78,7 @@ def load_numpy_array_data(file_path: str) -> np.array:
         with open(file_path, 'rb') as file_obj:
             return np.load(file_obj)
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
 
 
 
@@ -94,7 +94,7 @@ def save_object(file_path: str, obj: object) -> None:
         logging.info("Exited the save_object method of utils")
 
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
 
 
 
@@ -114,4 +114,4 @@ def drop_columns(df: DataFrame, cols: list)-> DataFrame:
         
         return df
     except Exception as e:
-        raise USvisaException(e, sys) from e
+        raise air_ProjectException(e, sys) from e
